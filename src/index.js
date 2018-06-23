f9c8e11ed4979b8c5726ba9027915e2677ea0d34
ghajl <michaelhnn@gmail.com>
2018-06-24 02:03:40 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -3,34 +3,34 @@ import ReactDOM from 'react-dom';
 import './style/main.scss';
 
 const Editor = () => {
-	return (
-		<textarea id="editor">editor</textarea>
-	)
+  return (
+    <textarea id="editor">editor</textarea>
+  )
 }
 
 const Preview = () => {
-	return (
-		<div id="preview">preview</div>
-	)
+  return (
+    <div id="preview">preview</div>
+  )
 }
 
 class App extends React.Component {
 
-	render() {
-		return (
-			<div className="container">
-				<div className="editor-wrapper">
-					<Editor />
-				</div>
-				<div className="preview-wrapper">
-					<Preview />
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="editor-wrapper">
+          <Editor />
+        </div>
+        <div className="preview-wrapper">
+          <Preview />
+        </div>
+      </div>
+    )
+  }
 }
 
 ReactDOM.render(
-	<App />,
-	document.getElementById('root')
+  <App />,
+  document.getElementById('root')
 )
