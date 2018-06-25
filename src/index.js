5a7b5f96526793bc7c9372e6a83ec76581c115af
ghajl <michaelhnn@gmail.com>
2018-06-26 00:06:38 +0300

    Add styling

--- src/index.js
+++ src/index.js
@@ -69,7 +69,11 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
+
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<App />, rootElement);
+} else {
+  ReactDOM.render(<App />, rootElement);
+}
