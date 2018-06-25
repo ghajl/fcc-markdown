ca75bb1ee1164f256b2be94d56c4d4ee639faf5b
ghajl <michaelhnn@gmail.com>
2018-06-25 23:54:50 +0300

    Add styling

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import marked from 'marked';
+import {stripIndent} from 'common-tags';
 import './style/main.scss';
 
 const Editor = (props) => {
-  return (
-    <textarea id="editor" className="pane" onChange={props.handleChange} value={props.output}></textarea>
-  )
+  return <textarea id="editor" className="pane" onChange={props.handleChange} value={props.output} autoFocus="true"></textarea>;
 }
 
 const Preview = (props) => {
-  
-  return (
-    <div id="preview" className="pane" dangerouslySetInnerHTML= {{__html: props.output}}></div>
-  )
+  return <div id="preview" className="pane" dangerouslySetInnerHTML= {{__html: props.output}}></div>;
 }
 
 class App extends React.Component {
@@ -23,7 +19,29 @@ class App extends React.Component {
   }
   
   componentDidMount() {
-    const initialValue = '# Level 1 Header\n## Level 2 Header\n[external link](http://google.com)\n\n`inline code`\n\n```a code block```,\n\n- list\n\n- item,\n\n\n > a blockquote,\n\n an image,\n\n **and bolded text.**';
+    const initialValue = 
+      stripIndent`
+      # Level 1 Header
+      ## Level 2 Header
+      [external link](http://google.com)
+
+      \`<div></div>\`
+
+      \`\`\`
+      const App = () => {
+        return <h1>Hello world!</h1>
+      }
+      \`\`\`
+
+      - list
+      - item
+
+      > a blockquote
+
+      ![alt text](https://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg "Logo Title Text 1")
+
+      and **bolded** text.
+      `;
     const previewOutput = marked(initialValue);
     this.setState({editor: initialValue, preview: previewOutput});    
   }
@@ -36,15 +54,13 @@ class App extends React.Component {
   
   render() {
     return (
-      <div className="container">
-        <header><h1>Markdown Previewer</h1></header>
-        <main>
-          <div className="pane-wrapper">
-            <header><span>Editor</span></header>
+      <div className="wrapper">
+        <header className="header"><h1>Markdown Previewer</h1></header>
+        <main className="app-container">
+          <div className="editor-wrapper">
             <Editor handleChange={this.handleChange} output={this.state.editor}/>
           </div>
-          <div className="pane-wrapper">
-            <header><span>Preview</span></header>
+          <div className="preview-wrapper">
             <Preview output={this.state.preview}/>
           </div>
         </main>
